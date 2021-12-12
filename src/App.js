91c7ec65aa32934049c50d58abf1b34abc7b1d25
TS93 <tschram93@yahoo.com>
2021-12-12 05:22:40 -0500

    began navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,16 @@
-import './Navbar.css';
+import React, { useState } from 'react';
 
+
+function Navbar() {
+    return ( 
+        <>
+            <nav className="navbar">
+                <div className='navbar-container'>
+                    
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
