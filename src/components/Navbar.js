2c2abab865f975942495b65db67719f51236f3e5
TS93 <tschram93@yahoo.com>
2021-12-12 05:52:33 -0500

    Navbar navbar switch between menu icons to the opposite

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
-function Navbar() {
+function Navbar() { 
+
+    const [ click, setClick ] = useState(false);
+
+    const handleClick = () => setClick(!click);
     return ( 
         <>
-            <nav className="navbar">
+            <nav className='navbar'>
                 <div className='navbar-container'>
                     <Link to="/" className="navbar-logo">
                         TRVL <i className="fab fa-typo3"></i>
                     </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
                 </div>
             </nav>
         </>
