f6651486a586b4e0861e1040e117c204eb065a56
TS93 <tschram93@yahoo.com>
2021-12-13 14:58:55 -0500

    added butons into the herosection component file with text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeroSection from './components/HeroSection';
 
 function App() {
 	return (
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Button } from './Button';
 
 function HeroSection() {
-    return (
-        <div className='hero-container'>
-            <video src='/videos/video-2.mp4' autoPlay loop muted />
-        </div>
-    )
+	return (
+		<div className="hero-container">
+			<video src="/videos/video-2.mp4" autoPlay loop muted />
+			<h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className='hero-btns'>
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--larger'>
+                    GET STARTED
+                </Button>
+            </div>
+		</div>
+	);
 }
 
-export default HeroSection
+export default HeroSection;
