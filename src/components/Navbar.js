6fe5573c16ebd4fb33bb197df4205d947cc14d08
TS93 <tschram93@yahoo.com>
2021-12-13 14:46:13 -0500

    fixed bug that would reload a button on mobile upon refresh page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Navbar.css';
@@ -18,6 +18,10 @@ function Navbar() {
         }
     }
         window.addEventListener('resize', showButton);
+
+    useEffect( () => {
+        showButton()
+    }, []);
 
 	return (
 		<>
