805eabfc8a59b5f1619a86070f28700abb8b12c8
TS93 <tschram93@yahoo.com>
2021-12-14 04:25:47 -0500

    begun SignUp page file

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,12 @@
+import react from ("react");
+import '../../App.css';
+
+function SignUp() {
+    return (
+        <h1 className='sign-up'>
+            SIGN UP
+        </h1>
+    )
+}
+
+export default SignUp
