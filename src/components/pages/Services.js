1942b3f70a6777de90f10a8bb0e985948c036b38
TS93 <tschram93@yahoo.com>
2021-12-14 04:24:15 -0500

    begun Services page file

---
+++ src/components/pages/Services.js
@@ -0,0 +1,12 @@
+import react from ("react");
+import '../../App.css';
+
+function Services() {
+    return (
+        <h1 className='services'>
+            SERVICES
+        </h1>
+    )
+}
+
+export default Services
