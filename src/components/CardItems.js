1da9b6173f8762b9c2a9fdafd9c0ec6b2c30232d
TS93 <tschram93@yahoo.com>
2021-12-14 04:04:13 -0500

    props added to carditems and used inside of cards.js

--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -6,11 +6,11 @@ function CardItem(props) {
 		<>
 			<li className="cards__item">
 				<Link className="cards__item__link" to={props.path}>
-                    <figure className='cards__item__pic-wrap'>
+                    <figure className='cards__item__pic-wrap' data-category={props.label}>
                         <img src={props.src} alt='Travel Image' className='cards__item__img'></img>
                     </figure>
                     <div className='cards__item__info'>
-                        <h5 className='cards__item__text'></h5>
+                        <h5 className='cards__item__text'>{props.text}</h5>
                     </div>
                 </Link>
 			</li>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,7 +9,12 @@ function Cards() {
 			<div className="cards__container">
 				<div className="cards__wrapper">
 					<ul className="cards__items">
-						<CardItems src="images/img-9.jpg" path="/services" />
+						<CardItems
+							src="images/img-9.jpg"
+							text="Explore the hidden waterfall deep inside the Amazon Jungle"
+							label="Adventure"
+							path="/services"
+						/>
 					</ul>
 				</div>
 			</div>
