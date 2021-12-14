205f03b331719143c780568c1e68f922602bf671
TS93 <tschram93@yahoo.com>
2021-12-14 05:16:56 -0500

    corrected error on Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,28 +4,27 @@ import './Footer.css';
 
 function Footer() {
 	return (
-		<div className="footer-container">
-			<section className="footer-subscription">
-				<p className="footer-subscription-heading">
-					Join the Adventure newsletter to receive our best vacation deals
-				</p>
-				<p className="footer-subscription-text">
-					You can unsubscribe at any time.
-				</p>
-				<div className="input-areas">
-					<form>
-						<input
-							className="footer-input"
-							type="email"
-							name="email"
-							placeholder="Your Email"
-						>
-							<Button buttonStyle="btn--outline">Subscribe</Button>
-						</input>
-					</form>
-				</div>
-			</section>
-		</div>
+        <div className='footer-container'>
+        <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+            Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className='footer-subscription-text'>
+            You can unsubscribe at any time.
+        </p>
+        <div className='input-areas'>
+            <form>
+            <input
+                className='footer-input'
+                name='email'
+                type='email'
+                placeholder='Your Email'
+            />
+            <Button buttonStyle='btn--outline'>Subscribe</Button>
+            </form>
+        </div>
+        </section>
+        </div>
 	);
 }
 
