cbfa4a7d2c71fef1d26c5d3e1f15a3ee3f457a6a
TS93 <tschram93@yahoo.com>
2022-01-27 19:31:43 -0500

    setting up server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const db = require('./config/connection');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+	});
+});
